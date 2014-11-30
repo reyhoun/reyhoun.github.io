36894aff044dd4bcb61fd10e128703d40d311ad6
Navid Kashani <navid.kashani@gmail.com>
2014-11-30 11:41:11 +0330

    first add fontawesome to webfont loader

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,5 +2,9 @@
 WebFont.load({
   google: {
     families: ['Open+Sans:400,300:latin', 'Expletus+Sans::latin']
+  },
+  custom: {
+    families: ['fontawesome'],
+    urls: ['../font/fonts.css']
   }
 });
