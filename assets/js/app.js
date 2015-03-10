3a8e8168ef473928364f2a797b4f760afd3bb8f3
Navid Kashani <navid.kashani@gmail.com>
2015-03-10 15:05:45 +0330

    Post: Fa: Colors in nature

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 // Loading Font from google
 WebFont.load({
   google: {
-    families: ['Raleway:600,300', 'Expletus+Sans::latin']
+    families: ['Raleway:600,300', 'Expletus+Sans::latin', 'Open+Sans:400,300:latin']
   },
   custom: {
     families: ['FontAwesome'],
